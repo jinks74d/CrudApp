8b279a565981cdb9536cd4f7b75927a950b55c2f
Raymond Jenkins <raymond@farsidedev.com>
2023-03-07 17:05:45 -0600

    complete helper functions

--- components/table.js
+++ components/table.js
@@ -1,7 +1,13 @@
 import { BiEdit, BiTrashAlt } from "react-icons/bi";
-import data from '../database/data.json'
+import { getUsers } from "../lib/helper";
+import { useQuery } from "react-query";
 
 export default function Table() {
+  const { isLoading, isError, data, error } = useQuery("users", getUsers);
+
+  if (isLoading) return <div>Employee is loading...</div>;
+  if (isError) return <div>Get error {error}</div>;
+
   return (
     <table className="min-w-full table-auto">
       <thead>
@@ -27,9 +33,9 @@ export default function Table() {
         </tr>
       </thead>
       <tbody className="bg-gray-200">
-        {
-          data.map((obj, i) => <Tr {...obj}key={i} />)
-        }
+        {data.map((obj, i) => (
+          <Tr {...obj} key={i} />
+        ))}
       </tbody>
     </table>
   );
@@ -39,7 +45,11 @@ function Tr({ id, name, avatar, email, salary, date, status }) {
   return (
     <tr className="bg-gray-50 text-center">
       <td className="px-16 py-2 flex flex-row items-center">
-        <img src={avatar || "#"} alt="/" />
+        <img
+          src={avatar || "#"}
+          alt="/"
+          className="h-8 w-8 rounded-full object-cover"
+        />
         <span className="text-center ml-2 font-semibold">
           {name || "Unknown"}
         </span>
@@ -61,7 +71,7 @@ function Tr({ id, name, avatar, email, salary, date, status }) {
       </td>
       <td className="px-16 py-2">
         <button className="cursor">
-          <span className="bg-green-500 text-white px-5 py-1 rounded-full">
+          <span className={`${status == "Active" ? 'bg-green-500':'bg-rose-500'} text-white px-5 py-1 rounded-full`}>
             {status || "Unknown"}
           </span>
         </button>
